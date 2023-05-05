6a14d7ca3dd30bf5c21cb03c979904b19e5a1c73
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-05-06 00:23:29 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,47 +1,31 @@
-fun main (){
-    names()
-    var cars = arrayOf("volvo","BMW","FORD","mazda")
-    car()
-    texts()
-    carss()
-    myFunction()
-}
-//NO.1
-//create a variable in kotlin that should store text and another store numbers
-fun names(){
-    var name ="john"
-    var birthyear=1935
-    println(name)
-    println(birthyear)
+
+fun main(){
+println(personDetails("yvonne",20,"kenyan"))
+    println(stringLength("tringle"))
+    myDetail("dan")
 
 }
-//No.2
-//find how many element an array of cars have
-fun car(){
-    var cars = arrayOf("volvo","BMW","FORD","mazda")
-    println(cars.contentToString().length)
 
-}
-//No.3
-//convert the string "Hello World" to upperCase
-fun texts(){
-    var text="Hello World"
-    println(text.toUpperCase())
-}
-//No.4
-//loop through  all element in the cars array.
-fun carss(){
-    var car = arrayOf("VOLVO","BMW","ford")
-    for(x in car){
-        println(x)
+//1. Write a function that takes in 3 parameters, name, age, and country, and returns a String with this structure “Hi, my name is x, I am y years old and I am from z.” Where x, y, and z are the provided name, age, and country respectively. (3 points)
+fun personDetails(name:String,age:Int,country:String):String{
+    return "Hi, my name is $name,I am $age years old and I am from $country"
 
-    }    }
-//No.5
-//create a function named"myFunction" that should output text i just got executed
-fun myFunction(){
-    var myFunctions="I just got executed"
-    println(myFunctions)
 }
 
 
+//2. Write a function that takes in a String and returns its length (2 points)
+fun stringLength(name:String):Int{
+    return name.length
+}
+
+//3. Write a function that takes in a name and prints out “That’s me!” when your name is passed to it, otherwise, it prints out “I don’t know who that is” (3 points)
+fun myDetail(name: String){
+    if (name=="yvonne"){
+        println("That's me")
+    }
+    else{
+        println("I don't know who that is ")
+    }
+}
+//4. Write a Kotlin function that takes in a string will all the vowels replaces by the character ‘*’. Use string interpolation to generate the output. (2 points)1. Write a function that takes in 3 parameters, name, age, and country, and returns a String with this structure “Hi, my name is x, I am y years old and I am from z.” Where x, y, and z are the provided name, age, and country respectively. (3 points)
 
